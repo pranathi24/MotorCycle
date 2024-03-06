aa07738813db51dfcf4a863578c10d47671d9834
NIKHIL479 <nikhilkacham15@gmail.com>
2024-03-06 15:46:21 -0500

    Nikhil changes

---
+++ MotorBrakes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motor
+{
+    public  class MotorBrakes
+    {
+        public void Brake()
+        {
+            Console.WriteLine("Applying brakes...");
+            CheckBrakes();
+            Console.WriteLine("Brakes applied. Motorcycle is slowing down.");
+        }
+
+        private void CheckBrakes()
+        {
+            Console.WriteLine("Checking brakes...");
+            Console.WriteLine("Brakes are in good condition.");
+        }
+    }
+}
